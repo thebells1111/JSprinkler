ea53f8d3ec2c986716a87adab8d5cdef678d1f05
Steven Bell <thebells1111@gmail.com>
2020-04-25 20:05:14 -0500

    added public folder

--- index.js
+++ index.js
@@ -1,70 +1,63 @@
-const Sprinkler = require('./sprinkler.js');
+const Sprinkler = require("./sprinkler.js");
 
 const sprinkler = new Sprinkler();
 sprinkler.init();
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
-const path = require('path');
+const path = require("path");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/static', express.static('public'))
-app.use(express.static('public'));
-app.use(cors())
+app.use(express.static("public"));
+app.use(cors());
 
-app.get('/programs', (req, res) => {
-	res.json(sprinkler);
+app.get("/programs", (req, res) => {
+  res.json(sprinkler);
 });
 
-app.get('/', (req, res) => {
-	 res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.get('/manual', (req, res) => {
-	 res.sendFile(path.join(__dirname + '/manual.html'));
-});
-
-app.post('/programs', (req, res) => {
+app.post("/programs", (req, res) => {
   let programs = req.body.programs;
 
-  if (programs){ 
-    sprinkler.updatePrograms(programs)   
-    .then(results=>{      
-      sprinkler.programs=results
-       Object.keys(sprinkler.stations).forEach(s => sprinkler.stations[s].duration = 0)
-      res.send('OK');
-    })
-    .catch(err=>{
-      res.status(500).send('Error-Program was not saved')
-    })
+  if (programs) {
+    sprinkler
+      .updatePrograms(programs)
+      .then((results) => {
+        sprinkler.programs = results;
+        Object.keys(sprinkler.stations).forEach(
+          (s) => (sprinkler.stations[s].duration = 0)
+        );
+        res.send("OK");
+      })
+      .catch((err) => {
+        res.status(500).send("Error-Program was not saved");
+      });
   } else {
-    res.status(400).send('Incorrect Syntax Used')
+    res.status(400).send("Incorrect Syntax Used");
   }
 });
 
-app.post('/manual', (req, res) => {
+app.post("/manual", (req, res) => {
   let programs = req.body.programs;
 
-  if (programs){ 
-    sprinkler.updateManual(programs) 
-    res.send('OK');    
+  if (programs) {
+    sprinkler.updateManual(programs);
+    res.send("OK");
   } else {
-    res.status(400).send('Incorrect Syntax Used')
+    res.status(400).send("Incorrect Syntax Used");
   }
 });
 
-app.post('/toggle_manual', (req, res) => {
-  sprinkler.manual = req.body.manual
-  if(req.body.manual){    
-    Object.keys(sprinkler.stations).forEach(s => {
-      sprinkler.stations[s].duration = 0
-    })
+app.post("/toggle_manual", (req, res) => {
+  sprinkler.manual = req.body.manual;
+  if (req.body.manual) {
+    Object.keys(sprinkler.stations).forEach((s) => {
+      sprinkler.stations[s].duration = 0;
+    });
   }
-  res.send('OK'); 
+  res.send("OK");
 });
 
-
-app.listen(80, () => console.log('server started'));
+app.listen(80, () => console.log("server started"));
--- sprinkler.js
+++ sprinkler.js
@@ -1,45 +1,45 @@
-const fs = require('fs');
-const rpio = require('rpio');
+const fs = require("fs");
+// const rpio = require('rpio');
 
 class Sprinkler {
   constructor() {
     this.manual = 0;
     this.stations = {
-      s1: { name: 'BROKEN', pin: 11, status: 0, duration: 0 },
-      s2: { name: 'Back', pin: 13, status: 0, duration: 0 },
-      s3: { name: 'Buckets', pin: 15, status: 0, duration: 0 },
-      s4: { name: 'Side', pin: 29, status: 0, duration: 0 },
-      s5: { name: 's5', pin: 31, status: 0, duration: 0 },
-      s6: { name: 's6', pin: 33, status: 0, duration: 0 },
-      s7: { name: 'Front', pin: 36, status: 0, duration: 0 },
-      s8: { name: 'Master', pin: 37, status: 0, duration: 0 },
+      s1: { name: "BROKEN", pin: 11, status: 0, duration: 0 },
+      s2: { name: "Back", pin: 13, status: 0, duration: 0 },
+      s3: { name: "Buckets", pin: 15, status: 0, duration: 0 },
+      s4: { name: "Side", pin: 29, status: 0, duration: 0 },
+      s5: { name: "s5", pin: 31, status: 0, duration: 0 },
+      s6: { name: "s6", pin: 33, status: 0, duration: 0 },
+      s7: { name: "Front", pin: 36, status: 0, duration: 0 },
+      s8: { name: "Master", pin: 37, status: 0, duration: 0 },
     };
     this.masterValve = {
-      station: 's8',
-      enable: 1
-    }
+      station: "s8",
+      enable: 1,
+    };
     this.programs = [];
   }
 
   init() {
-    Object.keys(this.stations).forEach(s => {
-      rpio.open(this.stations[s].pin, rpio.OUTPUT, rpio.LOW);
-    });  
-  
+    // Object.keys(this.stations).forEach(s => {
+    //   rpio.open(this.stations[s].pin, rpio.OUTPUT, rpio.LOW);
+    // });
+
     this.getPrograms();
     this.runPrograms();
-  };
-  
+  }
+
   masterValveControl() {
     let status = 0;
-    let duration = 0;    
+    let duration = 0;
 
-    Object.keys(this.stations).forEach(s => {
+    Object.keys(this.stations).forEach((s) => {
       if (s !== this.masterValve.station && this.stations[s].status === 1) {
         status = 1;
       }
       if (this.stations[s].duration > duration) {
-        duration = this.stations[s].duration
+        duration = this.stations[s].duration;
       }
     });
 
@@ -49,20 +49,19 @@ class Sprinkler {
     } else {
       this.turnOff(this.masterValve.station);
     }
-    
   }
 
   getPrograms() {
     try {
-      this.programs = JSON.parse(fs.readFileSync('./programs.json', 'utf8'));
-      this.programs.forEach(p => {
+      this.programs = JSON.parse(fs.readFileSync("./programs.json", "utf8"));
+      this.programs.forEach((p) => {
         p.timerOn = p.dailyStart;
         p.timerOff = p.dailyStart + p.timerDuration;
-      }); 
-      //this is to reset the on/off timers in case the program was 
-      //terminated prematurely    
+      });
+      //this is to reset the on/off timers in case the program was
+      //terminated prematurely
 
-      if (!this.programs[0].selectedStations){
+      if (!this.programs[0].selectedStations) {
         throw Error;
       }
     } catch (e) {
@@ -71,41 +70,40 @@ class Sprinkler {
     }
   }
 
-  runPrograms() { 
+  runPrograms() {
     if (this.manual) {
       this.checkManual();
     } else {
       this.checkPrograms();
     }
-    
-    if(this.masterValve.enable){
+
+    if (this.masterValve.enable) {
       this.masterValveControl();
     }
-    
-    setTimeout(this.runPrograms.bind(this), ((Math.floor(new Date().getTime() / 1000) + 1) * 1000) - new Date().getTime())
+
+    setTimeout(
+      this.runPrograms.bind(this),
+      (Math.floor(new Date().getTime() / 1000) + 1) * 1000 -
+        new Date().getTime()
+    );
 
     this.decreaseSprinklerDuration();
-  };
+  }
 
-  
-
-  checkPrograms() {    
+  checkPrograms() {
     let today = new Date(new Date().toLocaleDateString()).getTime();
     let timeNow = new Date().getTime();
-    let update = false; 
-    this.programs.forEach(p => {
-      if (p.type === 'DOW' && p.DOW.indexOf(new Date().getDay > -1)) {
-
+    let update = false;
+    this.programs.forEach((p) => {
+      if (p.type === "DOW" && p.DOW.indexOf(new Date().getDay > -1)) {
         p.dateStart = today;
-
-      } else {       
-        while (today > p.dateStart) {         
+      } else {
+        while (today > p.dateStart) {
           p.dateStart += p.dateInterval;
           update = true;
         }
       }
 
-      
       if (timeNow > p.dateStart + p.dailyStop) {
         p.dateStart += p.dateInterval;
         p.timerOn = p.dailyStart;
@@ -115,96 +113,107 @@ class Sprinkler {
 
       let nextOnTime = p.dateStart + p.timerOn;
       let nextOffTime = p.dateStart + p.timerOff;
-      
+
       while (timeNow > nextOffTime) {
         p.timerOff = p.timerOff + p.timerInterval;
         nextOffTime = p.dateStart + p.timerOff;
         p.timerOn = p.timerOff - p.timerDuration;
         nextOnTime = p.dateStart + p.timerOn;
-        update = true;      
-      };      
+        update = true;
+      }
 
       if (timeNow >= p.dateStart + p.dailyStart && timeNow >= nextOnTime) {
-        p.selectedStations.forEach(s => {
-          let duration = nextOffTime - (Math.round(timeNow / 1000) * 1000)
+        p.selectedStations.forEach((s) => {
+          let duration = nextOffTime - Math.round(timeNow / 1000) * 1000;
           if (duration > this.stations[s].duration) {
-            this.stations[s].duration = duration;                        
+            this.stations[s].duration = duration;
           }
         });
       }
 
-      Object.keys(this.stations).forEach(s => {
-        if (this.stations[s].duration > 0){
-          this.turnOn(s)
-        } 
+      Object.keys(this.stations).forEach((s) => {
+        if (this.stations[s].duration > 0) {
+          this.turnOn(s);
+        }
       });
-      
     });
-    
 
-    Object.keys(this.stations).forEach(s => this.turnOff(s))
+    Object.keys(this.stations).forEach((s) => this.turnOff(s));
 
     if (update) {
       this.updatePrograms(this.programs);
     }
-  };
-
-  
+  }
 
   decreaseSprinklerDuration() {
-    Object.keys(this.stations).forEach(s => {   
+    Object.keys(this.stations).forEach((s) => {
       if (this.stations[s].duration > 0) {
         this.stations[s].duration -= 1000;
       }
     });
-  };
+  }
 
   checkManual() {
-    Object.keys(this.stations).forEach(s => {
-      if (this.stations[s].duration > 0){
-        this.turnOn(s)
+    Object.keys(this.stations).forEach((s) => {
+      if (this.stations[s].duration > 0) {
+        this.turnOn(s);
       } else {
-        this.turnOff(s)
+        this.turnOff(s);
       }
-      
     });
   }
 
-  updateManual(programs){
-    Object.keys(this.stations).forEach(s => {
+  updateManual(programs) {
+    Object.keys(this.stations).forEach((s) => {
       this.stations[s].duration = programs[s];
     });
-  };
+  }
 
   turnOff(station) {
-    if (this.stations[station].status === 1 && this.stations[station].duration <= 0) {
-      console.log(`${this.stations[station].name} turned off at ${new Date().toLocaleString()}`)
+    if (
+      this.stations[station].status === 1 &&
+      this.stations[station].duration <= 0
+    ) {
+      console.log(
+        `${
+          this.stations[station].name
+        } turned off at ${new Date().toLocaleString()}`
+      );
       this.stations[station].status = 0;
-      rpio.write(this.stations[station].pin, rpio.LOW);
+      // rpio.write(this.stations[station].pin, rpio.LOW);
     }
-  };
+  }
 
   turnOn(station) {
     if (this.stations[station].status === 0) {
-      console.log(`${this.stations[station].name} turned on at ${new Date().toLocaleString()}`)
+      console.log(
+        `${
+          this.stations[station].name
+        } turned on at ${new Date().toLocaleString()}`
+      );
       this.stations[station].status = 1;
-      rpio.write(this.stations[station].pin, rpio.HIGH);
+      // rpio.write(this.stations[station].pin, rpio.HIGH);
     }
-  };
-
-  updatePrograms(data) { //this is a promise to handle changing the program from the clients browser in index.js
-    return new Promise(function(resolve, reject) {
-      fs.writeFile('./programs.json', JSON.stringify(data, null, 1), 'utf-8', function(err) {
-        if (err) {
-          console.log("Update Programs / Caught: " + e.message)
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      });
-    });    
   }
 
+  updatePrograms(data) {
+    //this is a promise to handle changing the program from the clients browser in index.js
+    return new Promise(function (resolve, reject) {
+      fs.writeFile(
+        "./programs.json",
+        JSON.stringify(data, null, 1),
+        "utf-8",
+        function (err) {
+          if (err) {
+            console.log("Update Programs / Caught: " + e.message);
+            reject(err);
+          } else {
+            resolve(data);
+          }
+        }
+      );
+    });
+  }
 }
 
 module.exports = Sprinkler;
